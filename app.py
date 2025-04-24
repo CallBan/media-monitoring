a7e0d23ec6979093a47e2a87c32d5fb2d264a180
DmitryKolbanov <kolbanov.dmitry@yandex.ru>
2025-04-24 14:49:05 +0300

    result page

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 app.secret_key = 'secret_key'
 NEWS_SOURCES = get_sources()
 # Для тестирования
-# news = [
-#     {'id': 123, 'url': 'https://ya.ru/', 'title': 'Загловок 1', 'content': 'Новость 1'},
-#     {'id': 456, 'url': 'https://ya.ru/', 'title': 'Загловок 2', 'content': 'Новость 2'}
-# ]
+news = [
+    {'id': 123, 'url': 'https://ya.ru/', 'title': 'Загловок 1', 'content': 'Новость 1'},
+    {'id': 456, 'url': 'https://ya.ru/', 'title': 'Загловок 2', 'content': 'Новость 2'}
+]
 
 @app.route('/', methods=['GET'])
 def index():
@@ -18,11 +18,11 @@ def index():
 
 @app.route('/search', methods=['POST'])
 def search():
-    keywords = request.form.get("keywords")
-    sources = request.form.getlist("sources")
-    date_range = request.form.get("date_range")
-    main = Main(sources=sources, keywords=keywords, date_range=date_range)
-    news = main.get_list_news()
+    # keywords = request.form.get("keywords")
+    # sources = request.form.getlist("sources")
+    # date_range = request.form.get("date_range")
+    # main = Main(sources=sources, keywords=keywords, date_range=date_range)
+    # news = main.get_list_news()
     return render_template("index.html", news_sources=NEWS_SOURCES, news=news)
 
 
