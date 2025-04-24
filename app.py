7a61c5e5411e5fcf46fd68af15c7d963b69cbd35
paschaGroznyj <pascha.soldatkin2015@yandex.ru>
2025-04-24 12:48:28 +0400

    ALL

--- app.py
+++ app.py
@@ -2,25 +2,27 @@ from flask import Flask, render_template, request
 from parser.main_parser import Main
 from parser.switch import get_sources
 
-app = Flask(__name__)
-app.secret_key = 'secret_key'
-NEWS_SOURCES = get_sources()
-
-
-@app.route('/search', methods=['POST'])
-def search():
-    keywords = request.form.get("keywords")
-    sources = request.form.getlist("sources")
-    date_range = request.form.get("date_range")
-    main = Main(sources=sources, keywords=keywords, date_range=date_range)
-    news = main.get_list_news()
-    return render_template("index.html", news_sources=NEWS_SOURCES, news=news)
-
-
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html', news_sources=NEWS_SOURCES, news=None)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+main = Main(['banki-ru'], '2025-04-22')
+main.export_to_excel([1, 2, 3])
+# app = Flask(__name__)
+# app.secret_key = 'secret_key'
+# NEWS_SOURCES = get_sources()
+#
+#
+# @app.route('/search', methods=['POST'])
+# def search():
+#     keywords = request.form.get("keywords")
+#     sources = request.form.getlist("sources")
+#     date_range = request.form.get("date_range")
+#     main = Main(sources=sources, keywords=keywords, date_range=date_range)
+#     news = main.get_list_news()
+#     return render_template("index.html", news_sources=NEWS_SOURCES, news=news)
+#
+#
+# @app.route('/', methods=['GET'])
+# def index():
+#     return render_template('index.html', news_sources=NEWS_SOURCES, news=None)
+#
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
